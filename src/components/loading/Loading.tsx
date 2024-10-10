7092da0b72528bcb86428dd8149c0978bda834b9
Linh Nguyen <linh.nguyen96114@gmail.com>
2024-10-10 11:40:26 +0700

    fix: style loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,29 +1,19 @@
 import { StyledComponentProps } from "@gluestack-style/react/lib/typescript/types";
-import { Box } from "@gluestack-ui/themed";
 import React from "react";
 import { ActivityIndicator, StyleProp, View, ViewProps, ViewStyle } from "react-native";
+
+import { Box } from "@/components";
 
 type LoadingProps = StyledComponentProps<StyleProp<ViewStyle>, unknown, ViewProps, "Box", typeof View> & {
     isLoading?: boolean;
 };
 
-const Loading: React.FC<LoadingProps> = ({ isLoading, ...rest }) => {
+const Loading: React.FC<LoadingProps> = ({ isLoading }) => {
     if (!isLoading) return null;
 
     return (
-        <Box
-            {...rest}
-            bg="rgb(44, 51, 51)"
-            opacity={0.7}
-            position="absolute"
-            zIndex={10}
-            top={0}
-            left={0}
-            right={0}
-            bottom={0}
-            justifyContent="center"
-            alignItems="center">
-            <Box w={100} h={100} borderRadius={10} bg="black" justifyContent="center" alignItems="center">
+        <Box className="bg-[rgb(44,51,51)] opacity-70 absolute z-10 top-0 left-0 right-0 bottom-0 flex justify-center items-center">
+            <Box className="w-[100px] h-[100px] rounded-2xl bg-black items-center justify-center">
                 <ActivityIndicator size="small" color="white" />
             </Box>
         </Box>
