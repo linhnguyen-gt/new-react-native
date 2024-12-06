1d003b61142aab5564e508655fbefec50b950ba2
Linh Nguyen <43808684+linhnguyen-gt@users.noreply.github.com>
2024-12-06 22:30:18 +0700

    feat: fix unit test

---
+++ jest.setup.js
@@ -0,0 +1,21 @@
+// require("@testing-library/jest-native/extend-expect");
+
+// // Mock the RN Platform
+// jest.mock("react-native/Libraries/Utilities/Platform", () => ({
+//     OS: "ios",
+//     select: jest.fn((obj) => obj.ios)
+// }));
+
+// // Mock AsyncStorage
+// jest.mock("@react-native-async-storage/async-storage", () => ({
+//     setItem: jest.fn(),
+//     getItem: jest.fn(),
+//     removeItem: jest.fn()
+// }));
+
+// // Mock react-native-config
+// jest.mock("react-native-config", () => ({
+//     Config: {
+//         API_URL: "https://api.test.com"
+//     }
+// }));
